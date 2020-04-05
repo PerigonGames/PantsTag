0ad53c575b5676a118ff248f0eee1a038b5141c0
Gursaanj <gursaanjresides@gmail.com>
2020-04-05 16:15:13 -0700

    Adding Walking Animation
    Added Walking Animation
    
    Setup Animator with IK Resolve

---
+++ Assets/Scripts/CharacterScripts/movementTrigger.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class MovementTrigger : MonoBehaviour
+{
+    [SerializeField] Animator m_animator;
+    [SerializeField] AnimationClip m_clip;
+
+
+    private void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            //Play Clip
+        }
+    }
+}
