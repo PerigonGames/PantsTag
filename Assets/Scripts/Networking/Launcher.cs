9631b29adab5edb35ca2f0367aa01e3f7e718538
wbeltran777 <williambeltran1997@gmail.com>
2020-04-05 11:56:48 -0700

    Launcher done

--- Assets/Scripts/Networking/Launcher.cs
+++ Assets/Scripts/Networking/Launcher.cs
@@ -2,56 +2,98 @@
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
+using Photon.Realtime;
 
-public class Launcher : MonoBehaviour
+namespace PerigonGames
 {
-    #region Private Serializable Fields
-
-
-    #endregion
-
-    #region Private Fields
-
-    /// <summary>
-    /// This client's version number. Users are separated from each other by gameVersion (which allow syou to make breaking changes).
-    /// </summary>
-    private string gameVersion = "1";
-
-    #endregion
-
-    #region MonoBehaviour CallBacks
-
-    void Awake()
+    public class Launcher : MonoBehaviourPunCallbacks
     {
-        // #Critical
-        // this makes sure we can use PhotonNetwork.LoadLevel() on the master and all clients in the same room sync their level automatically 
-        PhotonNetwork.AutomaticallySyncScene = true;
-    }
 
-    void Start()
-    {
-        Connect();
-    }
+        #region Private Serializable Fields
 
-    #endregion
+        [SerializeField]
+        private byte m_maxPlayersPerRoom = 4;
 
-    #region Public Methods
+        #endregion
 
-    public void Connect()
-    {
-        // Are we conencted?
-        if (PhotonNetwork.IsConnected)
+        #region Private Fields
+
+        /// <summary>
+        /// This client's version number. Users are separated from each other by gameVersion (which allow syou to make breaking changes).
+        /// </summary>
+        private string m_gameVersion = "1";
+
+        #endregion
+
+        #region MonoBehaviour CallBacks
+
+        void Awake()
         {
+            // #Critical
+            // this makes sure we can use PhotonNetwork.LoadLevel() on the master and all clients in the same room sync their level automatically 
+            PhotonNetwork.AutomaticallySyncScene = true;
+        }
+
+        void Start()
+        {
+            Connect();
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public void Connect()
+        {
+            if (PhotonNetwork.IsConnected)
+            {
+                PhotonNetwork.JoinRandomRoom();
+            }
+            else
+            {
+                PhotonNetwork.ConnectUsingSettings();
+                PhotonNetwork.GameVersion = m_gameVersion;
+            }
+        }
+
+        #region MonoBehaviourPunCallbacks
+
+        /// <summary>
+        /// If we connect succesfully then we fire this
+        /// </summary>
+        public override void OnConnectedToMaster()
+        {
+            // Cool we connected to master, so let's join a random room. 
             PhotonNetwork.JoinRandomRoom();
+            Debug.Log("PUN Basics Tutorial/Launcher: OnConnectedToMaster() was called by PUN");
         }
-        else
+
+        /// <summary>
+        /// If connection didn't work then oh no we fire this 
+        /// </summary>
+        /// <param name="cause"></param>
+        public override void OnDisconnected(DisconnectCause cause)
         {
-            PhotonNetwork.ConnectUsingSettings();
-            PhotonNetwork.GameVersion = gameVersion;
+            Debug.LogWarningFormat("PUN Basics Tutorial/Launcher: OnDisconnected() was called by PUN with reason {0}", cause);
         }
+
+        public override void OnJoinRandomFailed(short returnCode, string message)
+        {
+            Debug.Log("PUN Basics Tutorial/Launcher: OnJoinRandomRoomFailed() was called by PUN, no random room available");
+
+            // If we fail to join any room then we create our own new room
+            PhotonNetwork.CreateRoom(null, new RoomOptions { MaxPlayers = m_maxPlayersPerRoom});
+        }
+
+        public override void OnJoinedRoom()
+        {
+            Debug.Log("PUN basics tutorial?laucnher: OnjoinedRoom() called by PUN, now this client is in a room woot woot ");
+        }
+
+        #endregion
+
+
+        #endregion
+
     }
-
-
-    #endregion
-
 }
